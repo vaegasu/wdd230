4a770b684e9d5eaa8e31b6794cbc67f369567f3b
vaegasu <vaulaigasu@gmail.com>
2023-01-14 21:04:33 -0700

    js

---
+++ lesson2/scripts/design.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+ 
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+       
+    document.getElementById("updated").innerHTML = new Date()
+      });
