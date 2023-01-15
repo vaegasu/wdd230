91d7a079c58c2d45ae8e2d28a72cd28c7a413e31
vaegasu <vaulaigasu@gmail.com>
2023-01-14 19:11:42 -0700

    js

--- scripts/pp.js
+++ scripts/pp.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
  
-let year = document.querySelector("#year");
-const currentYear = new Date().getFullYear();
-year.textContent= currentYear;
-document.querySelector("#year").innerHTML = new Date().getFullYear();
+const currentYear = "2023";
+document.querySelector('#year').textContent = currentYear;
     
 document.getElementById("#updated").innerHTML = new Date()
   });
