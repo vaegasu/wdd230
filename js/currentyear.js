204880a45303e11d2e4a3fc7879b484758856521
vaegasu <vaulaigasu@gmail.com>
2023-04-22 17:48:25 -0600

    js

--- js/currentyear.js
+++ js/currentyear.js
@@ -1,2 +1,2 @@
 const options = {year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', options);
