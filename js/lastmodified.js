84303262ed6fef980511592238e14913fffcdadb
vaegasu <vaulaigasu@gmail.com>
2023-04-22 18:49:03 -0600

    js

--- js/lastmodified.js
+++ js/lastmodified.js
@@ -1,4 +1,19 @@
-function lastUpdated() {
-    let a = document.lastModified;
-    document.getElementById("lastmodified").innerHTML = a;
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
+        temp+" am"
+}
+
+function scrollDown(elem) {
+    if (elem.scrollTop < 100) {
+        document.querySelector('#scroll-down').className = "up"
+    }
+    else {
+        document.querySelector('#scroll-down').className = ""
+    }
 }
