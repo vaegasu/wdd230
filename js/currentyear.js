7e7813e75a91e67c7cb6b98c9967ea55be1a5f8f
vaegasu <vaulaigasu@gmail.com>
2023-04-26 23:13:04 -0600

    js

--- js/currentyear.js
+++ js/currentyear.js
@@ -1,2 +1,1 @@
-const options = {year: 'numeric'};
-document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("currentYear").innerHTML = new Date().getFullYear()
