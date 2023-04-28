13aa24766c4372d9cb190983bbc3c8b2997ecb5f
vaegasu <vaulaigasu@gmail.com>
2023-04-27 18:02:48 -0600

    js

--- lesson2/js/lastupdated.js
+++ lesson2/js/lastupdated.js
@@ -1,11 +1,11 @@
-var temp = document.lastUpdated
+var temp = document.lastModified
 var n = parseInt(temp.substring(temp.length-8,temp.length-6))
 if (n>12) {
-    document.getElementById("lastUpdated").innerHTML = "Last Updated: "+
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
         temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
 }
 else {
-    document.getElementById("lastUpdated").innerHTML = "Last Updated: "+
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
         temp+" am"
 }
 
