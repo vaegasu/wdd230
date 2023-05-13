952a0bccfc51113f7648385b6f197e52e4ad48fa
vaegasu <vaulaigasu@gmail.com>
2023-05-13 00:22:13 -0600

    chamber html

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -4,3 +4,28 @@ let date = document.getElementById("currentDate");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 date.innerHTML = fulldate;
+
+// Current Year 
+const currentYear = new Date();
+document.querySelector("#currentYear").textContent= currentYear.getFullYear();
+
+// Last Modified
+var temp = document.lastModified
+var n = parseInt(temp.substring(temp.length-8,temp.length-6))
+if (n>12) {
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
+        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
+}
+else {
+    document.getElementById("lastModified").innerHTML = "Last Updated: "+
+        temp+" am"
+}
+
+function scrollDown(elem) {
+    if (elem.scrollTop < 100) {
+        document.querySelector('#scroll-down').className = "up"
+    }
+    else {
+        document.querySelector('#scroll-down').className = ""
+    }
+}
