18ff8246bb233d3e462758fe85562f330b768c8c
vaegasu <vaulaigasu@gmail.com>
2023-05-12 23:52:26 -0600

    chamber html

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -1,1 +1,6 @@
+const datefield = document.querySelector("time");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
 
+datefield.textContent = fulldate;
