bf3d71c0279ec035bb48bf11c05f5ef20711fc4e
vaegasu <vaulaigasu@gmail.com>
2023-05-18 21:19:43 -0600

    dom-manipulation

---
+++ dom-manipulation/js/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#favchap");
+const button1 = document.querySelector("#addButton");
+const list = document.querySelector(".list");
+
+
+button1.addEventListener('click', ()=> {
+    let favchap = input.value;
+    const liAdd = document.createElement('li');
+    const bomAdd = document.createElement('span');
+    const dbAdd = document.createElement('button');
+
+    list.appendChild(bomAdd);
+    bomAdd.innerHTML = favchap;
+    list.appendChild(dbAdd);
+    dbAdd.innerHTML = 'X';
+    // dbAdd.setAttribute
+    list.appendChild(liAdd);
+    input.value = ' ';
+
+    dbAdd.addEventListener('click', () => {
+        list.removeChild(bomAdd);
+        list.removeChild(dbAdd);
+        list.removeChild(liAdd);
+
+    });
+});
+    input.focus();
