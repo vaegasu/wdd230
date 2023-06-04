285ba29e64022ab61facf123389a1d877e79328b
vaegasu <vaulaigasu@gmail.com>
2023-06-03 19:05:00 -0600

    lesson 7 html

--- lesson7/scripts/lazyload-script.js
+++ lesson7/scripts/lazyload-script.js
@@ -1,33 +1,28 @@
-const allImages = document.querySelectorAll("img[data-src]")
+const images = document.querySelectorAll("[data-src");
 
-const lazyLoad = (img) => {
-    img.setAttribute("src", img.getAttribute("data-src"))
-    img.onload = () => {
-        img.removeAttribute("data-src")
-        img.className = "in"
-    };
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
 };
 
-const options = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+function preloadImg(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+  img.src = src;
 }
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImg(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
 
-if ('IntersectionObserver' in window) {
-    const obsrvr = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                lazyLoad(item.target)
-                observer.unobserve(item.target)
-            }
-        })
-    }, options)
-    allImages.forEach((img) => {
-        obsrvr.observe(img)
-    })
-}
-else {
-    allImages.forEach((img) => {
-        lazyLoad(img)
-    })
-}
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
