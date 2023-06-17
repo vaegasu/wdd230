debe8bb02482a74688f6dfe552a8dd1183e70ca0
vaegasu <vaulaigasu@gmail.com>
2023-06-16 23:16:14 -0700

    Chamber - directory json

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,37 @@
+async function getBusinessDirectory() {
+    const response = await fetch("data/data.json");
+    const data = await response.json();
+    displayBusinessDirectory(data.business);
+}
+
+async function displayBusinessDirectory(business){
+    let card= document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let logo = document.createElement('img');
+
+    h2.innerHTML= `${member.name}`;
+    p1.innerHTML= `Address: ${member.address}`;
+    p2.innerHTML = `Phone: ${member.phone}`;
+    p3.innerHTML = `Web: ${member.site}`;
+    
+    logo.setAttribute("src", member.logo);
+    logo.setAttribute("alt", `${member.name} Logo`);
+    logo.setAttribute("loading", "lazy");
+    card.setAttribute("class", "dirMembers");
+
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+    card.appendChild(logo);
+    
+    cards.appendChild(card);
+}
+
+getBusinessDirectory();
+
+const tile = document.querySelector(".tile");
+const list = document.querySelector(".list");
