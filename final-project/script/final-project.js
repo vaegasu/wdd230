e1fe7f24468c731c3ee7255c1f7c7f825afb6f61
vaegasu <vaulaigasu@gmail.com>
2023-07-20 04:59:26 -0700

    final

--- final-project/script/final-project.js
+++ final-project/script/final-project.js
@@ -1,16 +1,21 @@
 const today = {month: 'long', day: 'numeric', year: 'numeric'};
 
+// weather //
+
 const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=38.9807&lon=-77.1003&units=imperial&appid=6029cdcec6e4ff661fe81b24b74ac429";
+
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsonObject) => {
   document.querySelector('#temp').textContent = jsonObject.current.temp;
   const iconsrc= `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`; 
-  const desc = jsonObject.current.weather.description;
+  const desc = jsonObject.weather[0].description;
   document.querySelector('#weather-icon').setAttribute('src', iconsrc);
   document.querySelector('#weather-icon').setAttribute('alt', desc);
   document.querySelector('#weatherDesc').textContent= desc; 
 })
+
+// fruits //
 
 const requestURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
 const cards = document.querySelector(".fruit-cards");
