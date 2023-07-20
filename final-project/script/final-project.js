f53ab8da20665ed97ab6df23a0d929b5600dc1a0
vaegasu <vaulaigasu@gmail.com>
2023-07-20 03:19:05 -0700

    final

--- final-project/script/final-project.js
+++ final-project/script/final-project.js
@@ -60,7 +60,7 @@ function displayTemples(fruit){
 
 function myMap() {
 var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
+  center:new google.maps.LatLng(33.6558, 117.8002),
   zoom:5,
 };
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
