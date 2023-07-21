87103bece60fdc8d61010444e29dc793159159f9
vaegasu <vaulaigasu@gmail.com>
2023-07-20 22:56:21 -0700

    chamber

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -43,3 +43,36 @@ x.onclick = toggleMenu;
 if (now.getDay() < 3 && now.getDay() > 0){
     document.getElementById("meetAndGreet").classList.toggle("activeBanner");
 }
+
+// lazy load //
+const images = document.querySelectorAll("[data-src]");
+
+function preLoadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src){
+        return;
+    }
+    
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting) {
+            return;
+        }
+        else {
+            preLoadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
