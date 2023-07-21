1b91f859b1a9259ed00da04c678a704313d9b24f
vaegasu <vaulaigasu@gmail.com>
2023-07-20 23:24:29 -0700

    chamber

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -76,3 +76,41 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 })
+
+// Weather API //
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5810988&units=imperial&appid=6029cdcec6e4ff661fe81b24b74ac429";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsonObject) => {
+    document.querySelector('#temp').textContent = jsonObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`; 
+    const desc = jsonObject.weather[0].description;
+    // desc = desc.split(' ').map(capitalize).join(' ');
+    const windspeed = jsonObject.wind.speed;
+    document.querySelector('#weather-icon').setAttribute('src', iconsrc);
+    document.querySelector('#weather-icon').setAttribute('alt', desc);
+    document.querySelector('#weatherDesc').textContent= desc; 
+    document.querySelector('#speed').textContent = windspeed;
+
+    let t = jsonObject.main.temp;   
+    let wc = '';
+
+      if (t <= 50 && windspeed > 3) {
+        wc = parseFloat(35.74 + (0.6215 * t) - (35.75 * Math.pow(windspeed,0.16)) + (0.4275 * t * Math.pow(windspeed,0.16))).toFixed(2);
+        document.getElementById('chill').innerHTML = `${wc} &#176;F`;
+      }
+      else {
+        document.getElementById('chill').innerHTML= 'N/A';
+        }
+  });
+
+// function capitalize(word) {
+//     return `${word.charAt(0).toUpperCase}${word.slice(1)}`;
+// }
+
+const listV = document.querySelector('.list');
+const tileV = document.querySelector('.tile');
+
+tileV.addEventListener('click', () => {dir-cards.classList.toggle('list')}, false);
+listV.addEventListener('click', ()=> {dir-cards.classList.toggle('tile')}, false); 
